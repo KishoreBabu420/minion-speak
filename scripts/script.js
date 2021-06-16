2a3a4a88971f1ebe34dddbf0cc94afae721523b8
Kishore <kishore.sai37@gmail.com>
2021-06-16 14:53:54 +0530

    taking user input

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let btnTranslate = document.getElementById('btn-translate');
+
+let userInput = document.getElementById('user-input');
+
+//functions
+
+const clickHandler = function () {
+  const textToTranslate = userInput.value;
+  console.log(textToTranslate);
+};
+
+btnTranslate.addEventListener('click', clickHandler);
